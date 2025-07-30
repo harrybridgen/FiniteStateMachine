903a108e743a95116cc00d471219219ab2b3ebbe
Harry Bridgen <105605342+BHarrry@users.noreply.github.com>
2025-07-30 04:33:16 +0100

    Update logic

--- Main.cs
+++ Main.cs
@@ -4,6 +4,8 @@
         vendingMachine.InsertCoin(20);
         vendingMachine.InsertCoin(50);
         vendingMachine.InsertCoin(20);
+        vendingMachine.InsertCoin(22);
+        vendingMachine.InsertCoin(20);
         vendingMachine.InsertCoin(20);
     }
 }
@@ -11,14 +13,12 @@
 public abstract class State {
     protected VendingMachine machine;
     public void SetMachine(VendingMachine machine) { this.machine = machine; }
-    public void OnEnter() { }
-    public void OnLeave() { }
+    public virtual void OnEnter() { }
     public virtual void InsertCoin(int amount) {
         if (!machine.IsValidAmount(amount)) {
             Console.WriteLine("Invalid coin! Returning...");
             return;
         }
-        Console.WriteLine("Machine is dispensing... Returning " + amount);
     }
 
 }
@@ -27,16 +27,13 @@ public class InsertCoinState : State {
     public InsertCoinState(VendingMachine machine) {
         base.SetMachine(machine);
     }
-    public void OnEnter() {
+    public override void OnEnter() {
         Console.WriteLine("The vending machine is asking for money!");
-    }
-
-    public void OnLeave() {
     }
 
     public override void InsertCoin(int amount) {
         if (!machine.IsValidAmount(amount)) {
-            Console.WriteLine("Invalid coin? 10,20,50 or 100!");
+            Console.WriteLine("Invalid coin? 10, 20, 50 or 100!");
             return;
         }
         if (machine.GetItemStock() <= 0) {
@@ -56,22 +53,17 @@ public class DispenseState : State {
     public DispenseState(VendingMachine machine) {
         base.SetMachine(machine);
     }
-    public void OnEnter() {
+    public override void OnEnter() {
         Console.WriteLine("The vending machine gives you an item!");
+        Console.WriteLine("Remaining stock: " + machine.GetItemStock());
         machine.AddAmount(-machine.GetItemPrice());
         machine.DecrementItemStock();
         machine.ChangeState(new GiveChangeState(machine));
     }
 
-    public void OnLeave() {
-    }
-
     public override void InsertCoin(int amount) {
-        if (!machine.IsValidAmount(amount)) {
-            Console.WriteLine("Invalid coin! Returning...");
-            return;
-        }
-        Console.WriteLine("Machine is dispensing... Returning " + amount);
+        base.InsertCoin(amount);
+        Console.WriteLine("Machine is busy... Returning " + amount);
     }
 }
 
@@ -79,23 +71,17 @@ public class GiveChangeState : State {
     public GiveChangeState(VendingMachine machine) {
         base.SetMachine(machine);
     }
-    public void OnEnter() {
+    public override void OnEnter() {
         if (machine.GetAmount() > 0) {
-            Console.WriteLine("Vending Machine gives you " + machine.GetAmount());
+            Console.WriteLine("Vending Machine gives you " + machine.GetAmount() + " back");
             machine.AddAmount(-machine.GetAmount());
         }
         machine.ChangeState(new InsertCoinState(machine));
     }
 
-    public void OnLeave() {
-    }
-
     public override void InsertCoin(int amount) {
-        if (!machine.IsValidAmount(amount)) {
-            Console.WriteLine("Invalid coin! Returning...");
-            return;
-        }
-        Console.WriteLine("Machine is giving change... Returning " + amount);
+        base.InsertCoin(amount);
+        Console.WriteLine("Machine is busy... Returning " + amount);
     }
 }
 
@@ -107,7 +93,7 @@ public class VendingMachine {
     private State currentState;
     private int currentAmount = 0;
     private int itemPrice = 50;
-    private int itemStock = 5;
+    private int itemStock = 2;
     public int GetAmount() { return this.currentAmount; }
     public void AddAmount(int amount){ this.currentAmount += amount; }
     public int GetCurrentAmount() { return currentAmount; }
@@ -123,7 +109,7 @@ public class VendingMachine {
     }
 
     public void ChangeState(State newState) {
-        if (this.currentState != null) { this.currentState.OnLeave(); }
+
         this.currentState = newState;
         this.currentState.OnEnter();
     }
